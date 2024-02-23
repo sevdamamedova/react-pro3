d90b0fb2fe8a2677620b2aca66f6f5122f66e3e4
sevdamamedova <msevda403@gmail.com>
2024-02-23 08:22:07 +0300

    remove stars with a

--- react-pro3/src/pages/AlionFollowers/index.jsx
+++ react-pro3/src/pages/AlionFollowers/index.jsx
@@ -2,26 +2,30 @@ import React, { useEffect, useReducer } from 'react'
 import PageContainer from '../../components/PageContainer'
 import alion_follower_reducer, { _action, initialStore } from './reducer'
 
-const style = {display: 'flex', gap: '20px', alignItems: 'center'}
+const style = {display: 'flex', gap: 10, alignItems: 'center'}
+const omgStyle = {width: 100, heigth: 100, objectFit: 'cover'}
 
 const AlionFollowers = () => {
     const [store, dispatch] = useReducer(alion_follower_reducer, initialStore)
     
     useEffect(() =>{
-        getFollowers().then((data) =>{
+        (async () =>{
+            const data = await getFollowers()
             dispatch({type: _action.get_followers, payload: data})
-        })       
+        })()      
     },[])
 
   return (
     <PageContainer>
             <h1> Wellocome to Alion's Followers pages</h1>
+            <button onClick={() => dispatch({type: _action.RM_first_with_a})}>remove first follover with a/A</button>
             {
                 store.followers.map((fol) =>{
                     return(
                         <div key={fol.id}>
                             <h1 style={style}>{fol.login} <button onClick={() => dispatch({type:_action.get_followers, payload: fol.id})}>X</button></h1>
-
+                            <button onClick={() => dispatch({type: _action.RM_last_follower})}>Remove Last Followers</button>
+                            <img style={omgStyle} src={fol.avatar_url}  alt={`${fol.login}'s image`}/>
                         </div>
                     )
                 })
--- react-pro3/src/pages/AlionFollowers/reducer.jsx
+++ react-pro3/src/pages/AlionFollowers/reducer.jsx
@@ -10,6 +10,8 @@ const _url = 'https://api-githup.com/users/Ali-GreenHeart/followers'
 export const _action = {
     'get_followers': 0,
     'remove_follower': 1,
+    'RM_last_follower' : 2,
+    'RM_first_with_a': 3,
 }
 
 const alion_follower_reducer = (store, action) => {
@@ -18,6 +20,22 @@ const alion_follower_reducer = (store, action) => {
             return {...store, followers: action.payload}
         case _action.remove_follower:
             return {...store, followers: store.followers.filter(({id}) => id !== action.payload)}
+        case _action.RM_last_follower:
+            return {...store, followers: store.followers.slice(0, store.followers.length - 1)}
+        case _action.RM_first_with_a:
+            let isFiltered = false
+            const newFols = store.followers.filter((fol) =>{
+        if(!isFiltered && fol.login.toLowerCase().startsWith('a')){
+            isFiltered = true
+            return false;
+        }
+        return true;
+            }) 
+            return {...store, followers: newFols}
+            // const index = store.followers.findIndex((fol) => fol.login.toLowerCase().startsWith('a'))
+            // const fols = [...store.followers]
+            // fols.splice(index, 1)
+            // return {...store, followers: fols}   
             default: 
             return store;
     }
